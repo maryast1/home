4a953df911677022c558c86ff2dae3ad8ce76ca9
maryast1 <152270273+maryast1@users.noreply.github.com>
2024-04-29 01:18:50 +1200

    add file Program1 (просто как дополнение)

---
+++ Program1.cs
@@ -0,0 +1,10 @@
+﻿// Можно сделать вот так. Буквально в несколько строчек
+
+
+string[] strings= {"hello", "888", "world", "string","two", "2", "on", "5", "off"}; 
+Console.Write($"[{String.Join(", ", strings)}]");
+Console.WriteLine();
+
+string[] result = strings.Where(x=>x.Length<=3).ToArray();
+
+Console.Write($"[{String.Join(", ", result)}]");
